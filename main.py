e1049b8bba5a4dce34c38f65e2a1fdb8c8a90063
Mubashra <mubashra747@gmail.com>
2026-02-16 02:30:23 +0500

    Add UCS,DLS,IDDFS and Bi-directional Search

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import random
 from collections import deque
-
+import heapq
 # Initialize pygame
 pygame.init()
 
@@ -198,5 +198,147 @@ def dfs(grid, start, end):
 
     return False
 
+# Add uCS
+def ucs(grid, start, end):
+    heap = []
+    heapq.heappush(heap, (0, start))
+    costs = {(start.row, start.col): 0}
 
+    directions = [
+        (-1, 0), (0, 1), (1, 0), (1, 1),
+        (0, -1), (-1, -1), (-1, 1), (1, -1)
+    ]
+
+    while heap:
+
+        pygame.time.delay(50)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        cost, current = heapq.heappop(heap)
+
+        if current == end:
+            reconstruct_path(start, end)
+            return True
+
+        for dr, dc in directions:
+            r = current.row + dr
+            c = current.col + dc
+
+            if 0 <= r < ROWS and 0 <= c < ROWS:
+                neighbor = grid[r][c]
+
+                move_cost = 1
+                new_cost = cost + move_cost
+
+                if neighbor.color != BLACK:
+                    if (r, c) not in costs or new_cost < costs[(r, c)]:
+                        costs[(r, c)] = new_cost
+                        neighbor.parent = current
+                        heapq.heappush(heap, (new_cost, neighbor))
+                        neighbor.color = YELLOW
+
+        if current != start:
+            current.color = RED
+
+        draw(grid)
+
+    return False
+
+#Add DLS
+def dls(grid, start, end, limit):
+    stack = [(start, 0)]
+    visited = set()
+
+    directions = [
+        (-1, 0), (0, 1), (1, 0), (1, 1),
+        (0, -1), (-1, -1), (-1, 1), (1, -1)
+    ]
+
+    while stack:
+        pygame.time.delay(50)
+
+        current, depth = stack.pop()
+
+        if current == end:
+            reconstruct_path(start, end)
+            return True
+
+        if depth <= limit:
+            visited.add((current.row, current.col))
+
+            for dr, dc in directions:
+                r = current.row + dr
+                c = current.col + dc
+
+                if 0 <= r < ROWS and 0 <= c < ROWS:
+                    neighbor = grid[r][c]
+
+                    if (r, c) not in visited and neighbor.color != BLACK:
+                        neighbor.parent = current
+                        neighbor.color = YELLOW
+                        stack.append((neighbor, depth + 1))
+
+        if current != start:
+            current.color = RED
+
+        draw(grid)
+
+    return False
+
+# Add IDDFS
+def iddfs(grid, start, end, max_depth=20):
+    for depth in range(max_depth):
+        for row in grid:
+            for node in row:
+                if node.color not in (BLACK, GREEN, BLUE):
+                    node.color = WHITE
+                node.parent = None
+        draw(grid)
+
+        if dls(grid, start, end, depth):
+            return True
+    return False
+
+# Add Bi-Directional Search
+def bidirectional(grid, start, end):
+    queue_start = deque([start])
+    queue_end = deque([end])
+
+    visited_start = { (start.row, start.col): start }
+    visited_end = { (end.row, end.col): end }
+
+    directions = [
+        (-1, 0), (0, 1), (1, 0), (1, 1),
+        (0, -1), (-1, -1), (-1, 1), (1, -1)
+    ]
+
+    while queue_start and queue_end:
+
+        pygame.time.delay(50)
+
+        current_start = queue_start.popleft()
+
+        for dr, dc in directions:
+            r = current_start.row + dr
+            c = current_start.col + dc
+
+            if 0 <= r < ROWS and 0 <= c < ROWS:
+                if (r, c) in visited_end:
+                    reconstruct_path(start, end)
+                    return True
+
+                neighbor = grid[r][c]
+                if (r, c) not in visited_start and neighbor.color != BLACK:
+                    visited_start[(r, c)] = neighbor
+                    neighbor.parent = current_start
+                    neighbor.color = YELLOW
+                    queue_start.append(neighbor)
+
+        draw(grid)
+
+    return False
 
